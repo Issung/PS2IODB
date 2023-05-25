96c8e386ca0330906d8b2b6e1d00fdf7ffa8dcfb
Issung <joel@sonofgoran.com>
2023-05-25 21:05:27 +1000

    Update python app to have icon export routine and export all icons in a save file with unique names

--- mymcplus source/mymcplus/gui/gui.py
+++ mymcplus source/mymcplus/gui/gui.py
@@ -543,125 +543,125 @@ class GuiFrame(wx.Frame):
         print("Entered Text:", entered_text)
         dialog.Destroy()
 
-        iconsys = self.icon_win._icon_sys
-        icon_normal_name = iconsys.icon_file_normal
-        icon_copy_name = iconsys.icon_file_copy
-        icon_delete_name = iconsys.icon_file_delete
-        icon_normal = self.icon_win._icon_normal
-
         if not os.path.exists(f"icon_exports/{entered_text}"):
             os.makedirs(f"icon_exports/{entered_text}")
-        path = f"icon_exports/{entered_text}/{icon_normal_name}"
 
-        with open(f"{path}.obj", 'w') as obj:
+        iconsys = self.icon_win._icon_sys
+
+        # Place names of the icon files into a dictionary, removing duplicates.
+        icon_dict = {
+            iconsys.icon_file_normal: self.icon_win._icon_normal,
+            iconsys.icon_file_copy: self.icon_win._icon_copy,
+            iconsys.icon_file_delete: self.icon_win._icon_delete,
+        }
+
+        for icon_filename in icon_dict:
+            self.export_icon(f"icon_exports/{entered_text}/", icon_filename, icon_dict[icon_filename])
+
+        iconsysoutput = {
+            "normal": iconsys.icon_file_normal,
+            "copy": iconsys.icon_file_copy,
+            "delete": iconsys.icon_file_delete,
+        }
+        with open(f"icon_exports/{entered_text}/iconsys.json", 'w') as file:
+            output = json.dumps(iconsysoutput, indent = 4, separators = (',', ':'))
+            file.write(output)
+        print(f"Wrote iconsys.json")
+
+    def export_icon(self, path, icon_filename, icon):
+        full_path_without_extension = f"{path}{icon_filename}"
+        # Write OBJ
+        with open(f"{full_path_without_extension}.obj", 'w') as obj:
             obj.write("# OBJ file\n")
             # Output 'mtllib' row (which material library to load materials from)
-            obj.write(f"mtllib {icon_normal_name}.mtl\n")
+            obj.write(f"mtllib {icon_filename}.mtl\n")
             # Output 'v' rows (vertices positions).
-            range_x = abs(min(icon_normal.vertex_data[0::3]) - max(icon_normal.vertex_data[0::3]))
-            range_y = abs(min(icon_normal.vertex_data[1::3]) - max(icon_normal.vertex_data[1::3]))
-            range_z = abs(min(icon_normal.vertex_data[2::3]) - max(icon_normal.vertex_data[2::3]))
+            range_x = abs(min(icon.vertex_data[0::3]) - max(icon.vertex_data[0::3]))
+            range_y = abs(min(icon.vertex_data[1::3]) - max(icon.vertex_data[1::3]))
+            range_z = abs(min(icon.vertex_data[2::3]) - max(icon.vertex_data[2::3]))
             range_max = max([range_x, range_y, range_z])
-            for vertex_index in range(icon_normal.vertex_count):
-                vertex_x = -icon_normal.vertex_data[vertex_index * 3] / range_max
-                vertex_y = -icon_normal.vertex_data[vertex_index * 3 + 1] / range_max
-                vertex_z = icon_normal.vertex_data[vertex_index * 3 + 2] / range_max
+            for vertex_index in range(icon.vertex_count):
+                vertex_x = -icon.vertex_data[vertex_index * 3] / range_max
+                vertex_y = -icon.vertex_data[vertex_index * 3 + 1] / range_max
+                vertex_z = icon.vertex_data[vertex_index * 3 + 2] / range_max
                 obj.write(f"v {vertex_x:.6f} {vertex_y:.6f} {vertex_z:.6f}\n")
             # Output 'vt' rows (UV mappings)
-            for uv_index in range(icon_normal.vertex_count):
-                u = round(icon_normal.uv_data[uv_index * 2] / 4096, 6)   # Divide by 4096 because that's supposedly the max u/v value?
-                v = round(icon_normal.uv_data[uv_index * 2 + 1] / 4096, 6)
+            for uv_index in range(icon.vertex_count):
+                u = round(icon.uv_data[uv_index * 2] / 4096, 6)   # Divide by 4096 because that's supposedly the max u/v value?
+                v = round(icon.uv_data[uv_index * 2 + 1] / 4096, 6)
                 obj.write(f"vt {u:.6f} {v:.6f} 0.000000\n")
             # Output 'vn' rows (vertex normals)
-            for normal_index in range(icon_normal.vertex_count):
-                normal_x = icon_normal.vertex_normals[normal_index * 3]
-                normal_y = icon_normal.vertex_normals[normal_index * 3 + 1]
-                normal_z = icon_normal.vertex_normals[normal_index * 3 + 2]
+            for normal_index in range(icon.vertex_count):
+                normal_x = icon.vertex_normals[normal_index * 3]
+                normal_y = icon.vertex_normals[normal_index * 3 + 1]
+                normal_z = icon.vertex_normals[normal_index * 3 + 2]
                 obj.write(f"vn {normal_x:.6f} {normal_y:.6f} {normal_z:.6f}\n")
             # Output 'usemtl' row (which material to use for the following faces)
             obj.write("usemtl Texture\n")
             # Output 'f' rows (faces connected to which vertices).
-            for face_index in range(int(icon_normal.vertex_count / 3)):
+            for face_index in range(int(icon.vertex_count / 3)):
                 v1 = face_index * 3 + 1
                 v2 = face_index * 3 + 2
                 v3 = face_index * 3 + 3
                 obj.write(f"f {v1}/{v1}/{v1} {v2}/{v2}/{v2} {v3}/{v3}/{v3}\n")
+        print(f"Wrote {full_path_without_extension}.obj")
 
-        print(f"Wrote {path}.obj")
-
-        with open(f"{path}.mtl", 'w') as mtl:
+        # Write MTL
+        with open(f"{full_path_without_extension}.mtl", 'w') as mtl:
             mtl.write("newmtl Texture\n")
-            mtl.write(f"map_Kd {icon_normal_name}.png\n")
+            mtl.write(f"map_Kd {icon_filename}.png\n")
+        print(f"Wrote {full_path_without_extension}.mtl")
 
-        print(f"Wrote {path}.mtl")
-
+        # Write PNG
         image = Image.new('RGB', (128, 128), color='black')
-
         step_size = 2
-        for i in range(0, len(icon_normal.texture), step_size):
+        for i in range(0, len(icon.texture), step_size):
             x = int((i / step_size) % 128)
             y = 127 - int((i / step_size) / 128)
-            col = reduce(lambda a, b: ((a) << 8) | b, icon_normal.texture[i:i+step_size][::-1]) # [::-1] reverses the array 2 element array.
+            col = reduce(lambda a, b: ((a) << 8) | b, icon.texture[i:i+step_size][::-1]) # [::-1] reverses the array 2 element array.
             r = (col & 0x1F) << 3
             g = ((col >> 5) & 0x1F) << 3
             # This blue channel part differs from the c++ implementation because we can't force to an unsigned byte.
             # For some reason this channel for compressed icons will always be 0b10000000 making blue always max, giving everything a blue tint.
             b = (((col >> 10)) << 3) & 0xFF 
             a = 255
-            #print(f"trying x{x}, y{y}. col: hex:{hex(col)}, a{a}, r{r}, g{g}, b{b}")
-            #if i == (len(icon.texture) - step_size):
-            #    print("this is the last pixel, which should be the top right.")
             image.putpixel((x, y), (r, g, b, a))
+        image.save(f'{full_path_without_extension}.png', 'PNG')
+        print(f"Wrote {full_path_without_extension}.png")
 
-        image.save(f'{path}.png', 'PNG')
-
-        print(f"Wrote {path}.png")
-
-        frames = int(len(icon_normal.vertex_data) / icon_normal.vertex_count / 3)
-        if icon_normal.anim_header.frame_count <= 1:
+        # Write ANIM (if required).
+        frames = int(len(icon.vertex_data) / icon.vertex_count / 3)
+        if icon.anim_header.frame_count <= 1:
             print("Not writing animation file because only 1 frame")
         else:
             anim_data = {
-                "frameLength": icon_normal.anim_header.frame_length,
-                "animSpeed": icon_normal.anim_header.anim_speed,
-                "playOffset": icon_normal.anim_header.play_offset,
+                "frameLength": icon.anim_header.frame_length,
+                "animSpeed": icon.anim_header.anim_speed,
+                "playOffset": icon.anim_header.play_offset,
                 "frames": [],
             }
             for frame_index in range(frames):
                 frame = { "keys" : [], "vertexData": SingleLineList([]) }
-                v_from = frame_index * (icon_normal.vertex_count * 3)
-                v_to = (frame_index + 1) * (icon_normal.vertex_count * 3)
-                frame["vertexData"] = SingleLineList(icon_normal.vertex_data[v_from:v_to])
+                v_from = frame_index * (icon.vertex_count * 3)
+                v_to = (frame_index + 1) * (icon.vertex_count * 3)
+                frame["vertexData"] = SingleLineList(icon.vertex_data[v_from:v_to])
                 # Normalise all vertex coords so everything isn't too large (0.0 - 1.0) is ideal.
                 for i in range(len(frame["vertexData"])):
                     frame["vertexData"][i] = frame["vertexData"][i] / range_max
 
                 # Write key data.
-                for key_index in range(icon_normal.frames[frame_index].key_count):
+                for key_index in range(icon.frames[frame_index].key_count):
                     frame["keys"].append(SingleLineObject({
-                        "time": icon_normal.frames[frame_index].keys[key_index].time,
-                        "value": icon_normal.frames[frame_index].keys[key_index].value,
+                        "time": icon.frames[frame_index].keys[key_index].time,
+                        "value": icon.frames[frame_index].keys[key_index].value,
                     }))
 
                 anim_data["frames"].append(frame)
-                    
-            with open(f"{path}.anim", 'w') as file:
+            with open(f"{full_path_without_extension}.anim", 'w') as file:
                 output = json.dumps(anim_data, indent = 4, separators = (',', ':'), cls = CustomJSONEncoder)
                 output = output.replace('"##<', "").replace('>##"', "").replace("'", '"')
                 file.write(output)
-                
-            print(f"Wrote {path}.anim ({frames} frames)")
-
-        iconsysoutput = {
-            "normal": icon_normal_name,
-            "copy": icon_copy_name,
-            "delete": icon_delete_name,
-        }
-        with open(f"icon_exports/{entered_text}/iconsys.json", 'w') as file:
-            output = json.dumps(iconsysoutput, indent = 4, separators = (',', ':'))
-            file.write(output)
-        print(f"Wrote iconsys.json.")
-
+            print(f"Wrote {full_path_without_extension}.anim ({frames} frames)")
 
     def evt_cmd_exit(self, event):
         self.Close(True)
