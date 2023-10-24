d18a51faaa260e82b7790cfbf160211ecf833f93
Issung <joel@sonofgoran.com>
2023-10-24 20:15:54 +1100

    #52 Restore missing `runme.py` python file

--- mymc++/build.sh
+++ mymc++/build.sh
@@ -1,6 +1,7 @@
 # Script to build MYMC++ to a single executable file.
 # It is possible you get a "virus or potentially unwanted software error", just disable Windows Defender real time protection temporarily.
-# This will output an executable to "dist" folder.
+# Temporary files are stored in the "build" folder.
+# A single executable file will be created in the "dist" folder.
 
 # If --noconsole is used then whenever python tries to write to the console the app will crash, 
 
