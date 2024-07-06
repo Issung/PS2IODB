2ddc7b6c76dfadeec09cca3d70da21fa294460a4
Issung <joel@sonofgoran.com>
2024-07-06 15:32:18 +1000

    Add icons contributed by Typedesigns

--- website/src/model/Contributors.tsx
+++ website/src/model/Contributors.tsx
@@ -4,4 +4,5 @@ export class Contributors {
     public static Issung = new Contributor('Issung', 'https://x.com/IssunGee');
     public static Rikineko = new Contributor('rikineko');
     public static Idiot = new Contributor('IdiOT');
+    public static Typedesigns = new Contributor('Typedesigns', 'https://x.com/typedesigns_');
 }
