c5a7064f33fea375260941eb2ec4e1a2180a0102
Issung <joel@sonofgoran.com>
2024-07-07 14:29:49 +1000

    Upload Travis Touchdown's icon contributions from the Discord

--- website/src/model/Contributors.tsx
+++ website/src/model/Contributors.tsx
@@ -5,4 +5,5 @@ export class Contributors {
     public static Rikineko = new Contributor('rikineko');
     public static Idiot = new Contributor('IdiOT');
     public static Typedesigns = new Contributor('Typedesigns', 'https://x.com/typedesigns_');
+    public static TravisTouchdown = new Contributor('Travis Touchdown', 'https://x.com/TravisTchDown');
 }
