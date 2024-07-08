54cda890ba9654bcf7c3f7582ee5ee53cf19e6cc
Issung <joel@sonofgoran.com>
2024-07-08 20:21:43 +1000

    Upload contributions from VibiLeFleu via Twitter
    https://x.com/VibiLeFleu

--- website/src/model/Contributors.tsx
+++ website/src/model/Contributors.tsx
@@ -6,4 +6,5 @@ export class Contributors {
     public static Idiot = new Contributor('IdiOT');
     public static Typedesigns = new Contributor('Typedesigns', 'https://x.com/typedesigns_');
     public static TravisTouchdown = new Contributor('Travis Touchdown', 'https://x.com/TravisTchDown');
+    public static VibiLeFleu = new Contributor('VibiLeFleu', 'https://x.com/VibiLeFleu');
 }
