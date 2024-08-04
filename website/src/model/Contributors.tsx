c502418096838c4ba767f7a1703f02ed530210c0
Issung <joel@sonofgoran.com>
2024-08-04 10:59:02 +1000

    Rename contributor IdiOT to NateTool / Nathan Tool

--- website/src/model/Contributors.tsx
+++ website/src/model/Contributors.tsx
@@ -3,7 +3,7 @@ import { Contributor } from "./Contributor";
 export class Contributors {
     public static Issung = new Contributor('Issung', 'https://x.com/IssunGee');
     public static Rikineko = new Contributor('rikineko');
-    public static Idiot = new Contributor('IdiOT');
+    public static NateTool = new Contributor('Nathan Tool', 'https://www.reddit.com/u/NateTool/');
     public static Typedesigns = new Contributor('Typedesigns', 'https://x.com/typedesigns_');
     public static TravisTouchdown = new Contributor('Travis Touchdown', 'https://x.com/TravisTchDown');
     public static VibiLeFleu = new Contributor('VibiLeFleu', 'https://x.com/VibiLeFleu');
