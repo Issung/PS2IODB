badcd5f06c3451452d6496cef6dc8988e3678c9d
Issung <joel@sonofgoran.com>
2024-09-22 11:53:34 +1000

    Set ps2romsfree's contributor link to their YouTube channel @BamhurgeVidNZuffs

--- website/src/model/Contributors.ts
+++ website/src/model/Contributors.ts
@@ -18,5 +18,5 @@ export class Contributors {
     public static SqueezedDog = new Contributor('Squeezed Dog');
     public static NBForever = new Contributor('N & B Forever'); // Uploaded by Squeezed Dog
     public static DrinkMoreWater = new Contributor('Drink More Water!'); // Uploaded by Squeezed Dog
-    public static Ps2RomsFree = new Contributor('ps2romsfree');
+    public static Ps2RomsFree = new Contributor('ps2romsfree', 'https://www.youtube.com/@BamhurgeVidNZuffs');
 }
