e374b29910a36b68b390b82c7600e8a4b93b19ce
Issung <joel@sonofgoran.com>
2024-09-24 20:33:40 +1000

    Fix GameTable not having unique key for each list item
    Had to add a key to the outer fragment, that is what's part of the list.

--- website/src/components/GameTable.tsx
+++ website/src/components/GameTable.tsx
@@ -15,17 +15,15 @@ const GameTable: React.FC<GameTableProps> = ({ games }: GameTableProps) => {
             <h4 style={{ textAlign: 'left' }}>{games.length === 0 ? 'No Results.' : `${games.length} Results`}</h4>
             <table>
                 <tbody>
-                    {/* TODO: Multiple icon variants are indented below the  */}
                     {games.map(game => {
                         if (game.icons.length > 1)
                         {
-                            return <>
+                            return <React.Fragment key={game.index}>
                                 <RowBase 
                                     title={game.name}
                                     contributed={game.icons.some(i => i.code)}
                                     circle={Trait.MultiIcon}
                                     tooltip="This title has multiple icons"
-                                    key={game.index}
                                 />
                                 <tr className="subicons">
                                     <td className="line">
@@ -36,7 +34,7 @@ const GameTable: React.FC<GameTableProps> = ({ games }: GameTableProps) => {
                                         </tbody>
                                     </table>
                                 </tr>
-                            </>
+                            </React.Fragment>
                         }
                         else
                         {
