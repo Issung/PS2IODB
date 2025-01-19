7fc1936daee685033f302d63c89c5b4c9e16de10
Issung <joel@sonofgoran.com>
2025-01-19 15:33:28 +1100

    Upload 6 contributions from new member pm41224 via the Discord

--- website/src/model/Contributors.ts
+++ website/src/model/Contributors.ts
@@ -27,6 +27,7 @@ export class Contributors {
     public static Cajas = new Contributor('Cajas');
     public static VideoGameKing = new Contributor('Video_Game_King');
     public static RyutoSetsujin = new Contributor('Ryuto Setsujin', 'https://bsky.app/profile/ryuto.quizmagic.academy');
+    public static Pm41224 = new Contributor('pm41224');
 }
 
 export const ContributorCount = Object.values(Contributors).length;
