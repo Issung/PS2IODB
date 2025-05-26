fb0aeb7ce594ad20a6cb12c8bab7a1530ba9023f
Issung <joel@sonofgoran.com>
2025-05-26 20:34:51 +1000

    Update contribution page to remove GitHub info & add guidelines

--- website/src/pages/Contribute.tsx
+++ website/src/pages/Contribute.tsx
@@ -117,25 +117,42 @@ const Contribute: React.FC = () => {
                             </ol>
 
                             <a id="iconuploading" href="#iconuploading">
+                                <h4>Icon Uploading</h4>
+                            </a>
+                            <p>Now that you have extracted icon assets from a memory card using the previous guides, you can now send the icons to be uploaded to the site.</p>
+                            <ul>
+                                <li>Most people upload their contributions to the <code>#icon-uploads</code> channel in the <a target="_blank" href="https://discord.gg/SWsuNvWnKw">PS2IODB Discord server</a>.</li>
+                                <li>If you don't want to join the Discord just send your icons zip to Issung via E-Mail at <a href="mailto:issun@sonofgoran.com">issun@sonofgoran.com</a>.</li>
+                            </ul>
+
+                            <h6>Icon Contribution Guidelines</h6>
+                            <ul>
+                                <li>Make sure when you are uploading icons that they haven't already been contributed!</li>
+                                <li>The PS2 library is massive and many games have regional differences please title each icon clearly, or add a text file with notes!</li>
+                                <li>For games that have multiple icons it is useful to know what conditions cause each case. If you don't know that's OK, just let us know, and hopefully someone later will inform us.</li>
+                                <li>Some icons are known to have issues exporting, these are useful to have for improving the extractor tool. Please send the PS2 memcard file either in the <code>#broken-icons</code> channel in the Discord or E-Mail to Issung.</li>
+                            </ul>
+
+                            {/*<a id="iconuploading" href="#iconuploading">
                                 <h4>Icon Uploading: GitHub PR</h4>
                             </a>
                             <p>Now that you have extracted icon assets from a memory card using the previous guides, this is the step where you contribute the assets to the PS2IODB.</p>
                             <p>When uploading an icon the Git flow looks like this:</p>
-                            <img src="http://placekitten.com/300/200" alt="Git contribution diagram"/> {/* TODO: Add legit diagram showing fork to pull request flow. */}
+                            <img src="http://placekitten.com/300/200" alt="Git contribution diagram"/> {// TODO: Add legit diagram showing fork to pull request flow.
                             <br/>
                             <br/>
 
                             <p>The steps are as follows:</p>
                             <ol>
-                                <li>Create a fork of the GitHub repository.</li> {/* TODO: Link to the repository with the correct URL. */}
+                                <li>Create a fork of the GitHub repository.</li> // TODO: Link to the repository with the correct URL.
                                 <li>Clone your forked repository to your machine.</li>
-                                <li>Copy the exported icon folder to the /////// folder, where the rest are.</li>  {/* TODO: Put correct folder name. */}
-                                <li>In <code>GameList.tsx</code> find the title you are contributing, add a string parameter of the folder name you just added, and a number for the unique amount of icons.</li> {/* TODO: Give path for the file. */}
+                                <li>Copy the exported icon folder to the /////// folder, where the rest are.</li>  // TODO: Put correct folder name.
+                                <li>In <code>GameList.tsx</code> find the title you are contributing, add a string parameter of the folder name you just added, and a number for the unique amount of icons.</li> // TODO: Give path for the file.
                                 <li>With your Git client stage your changes and make a commit with a helpful message, then push.</li>
-                                <li>Now you can create a pull request to merge the changes in your fork with the official repo.</li> {/* TODO: 'Create PR' in repo link. */}
+                                <li>Now you can create a pull request to merge the changes in your fork with the official repo.</li> // TODO: 'Create PR' in repo link.
                                 <li>After this is done the maintainers will inspect your changes and possibly request fixes or just accept them straight away.</li>
                                 <li>That's all you need to do! Thank you for contributing!</li>
-                            </ol>
+                            </ol>*/}
                         </div>
                     </div>
                 </div>
